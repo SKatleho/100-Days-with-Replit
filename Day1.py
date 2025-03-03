45b8c319a66a9345ef56ea6d8ddbbdc209cc17e0
Katleho Califonia <77230651+SKatleho@users.noreply.github.com>
2025-03-03 21:36:58 +0200

    Add files via upload
    https://replit.com/@katlehostuurman/day-1100-days?v=1

---
+++ Day1.py
@@ -0,0 +1,11 @@
+print("""KC Stuurman 
+03 March 2025
+I am signing up for Replit's 100 days of Python
+challenge!
+I will make sure to spend some time  every day coding 
+along, foe a minimun of 10 min a day.
+I'll bw using Replit, an amazing online IDE so I can do 
+this from my phone wherever I happen to be. No excuses
+for mot coding from the middle of a field!
+
+I am happy to join Replit😎""")
