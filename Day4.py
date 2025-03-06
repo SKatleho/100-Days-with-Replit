f1edfbb32e578f840860a842cdf2f34035a4ac6e
Katleho Califonia <77230651+SKatleho@users.noreply.github.com>
2025-03-06 08:45:21 +0200

    Add files via upload
    https://replit.com/@katlehostuurman/day4100-days?v=1

---
+++ Day4.py
@@ -0,0 +1,8 @@
+#Program: Student Information Program
+#Task: Write a program that asks the user for their name, age, and grade level, then prints out a personalized message with their information.
+
+name = input("What is your name : ")
+age = input("How old are you : ")
+grade = input("What grade are you in : ")
+print("")
+print("Hello, my name is " + name + ". I am " + age + " years old and in grade " + grade + ".")
