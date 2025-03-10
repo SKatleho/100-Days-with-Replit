e589c220a5d9a348f2a1766d7ad50e87889fd061
Katleho Califonia <77230651+SKatleho@users.noreply.github.com>
2025-03-10 21:01:19 +0200

    Add files via upload
    https://replit.com/@katlehostuurman/day5100-days?v=1

---
+++ Day5.py
@@ -0,0 +1,19 @@
+print("Marvel Movie Character Creator")
+print("--")
+spiderAnswer = input("Do you like 'hanging around'?: ")
+if spiderAnswer == "Yes":
+  print("You are Spider-man")
+else:
+  print("Then you're not Spider-man")
+print("")
+korgAnswer = input("Do you have a 'gravelly' voice?: ")
+if korgAnswer == "Yes":
+  print("You are Korg")
+else:
+  print("Aww, then you're not Korg")
+print("")
+marvelousAnswer = input("Do you often feel 'Marvelous'?: ")
+if marvelousAnswer == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("You are not Captain Marvel")
