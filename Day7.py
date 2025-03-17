cd19808eba8a07e5e158b4a4f934d08ed8b28ff4
Katleho Califonia <77230651+SKatleho@users.noreply.github.com>
2025-03-17 16:20:40 +0200

    Add files via upload
    https://replit.com/@katlehostuurman/day-7100-days?v=1

---
+++ Day7.py
@@ -0,0 +1,37 @@
+print("FAKE FAN FINDER🙄🙄😕")
+print("+++++++++++++++++++++++")
+answer1 = input("Are you a fan of marvel movies? ")
+if answer1 == "Yes" or "yes":
+    print(" ")
+    print("""Ohoo okay, I hope you are telling the truth because 
+I will find out if you are not honest with me.""")
+    print(" ")
+    answer2 = input("""Which character do you love the most between 
+<Captain America> or <Iron Man> marvel movies? """)
+    if answer2 == "Captain America":
+        answer3 = input("""How many captiain America character do marvel 
+have that we know of and we have seen them in action?  """)
+        if answer3 == "2":
+            answer4 = input("Clearly you know Cap ,last question.Give me the real name first Captain America? ")
+            if answer4 == "Steve Rogers":
+                print("My man 😎🤜🏽🤛🏼, I respect you.")
+            else:
+                print("Go and re-watch marvels again my guy👀👋🏼")
+        else:
+            print("Clearly you don't know Captain America as you thought😎🤣🤣")
+    elif answer2 == "Iron Man":
+        answer2p2 = input("Iron Man has how many movies? ")
+        if answer2p2 == "3":
+            answer2p3 = input(
+                "You are right 😎🤜🏽🤛🏼.Last one Iron man died in which movie :")
+            if answer2p3 == "EndGame":
+                print("My man 😎🤜🏽🤛🏼, I respect you.")
+            else:
+                print("Go and re-watch marvels again my guy👀👋🏼")
+        else:
+            print("Clearly you don't know Iron Man big brother😎🤣🤣")
+    else:
+        print("Clearly you don't know marvel characters big brother😎🤣🤣")
+
+else:
+    print("Thank you for coming clean early 🤜🏽🤛🏼")
